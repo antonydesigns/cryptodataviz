5c67c2cccbc44ed777a8900efff7939f1db0a64c
gio <gio>
2023-10-31 16:50:37 +0800

    fix page conflicts

--- app/(marketing)/(homepage)/page.tsx
+++ app/(marketing)/(homepage)/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Crypto Data Viz is still under construction :)</p>
       <p>Please come back later!</p>
-      <p>this shouldn't exist in main</p>
     </main>
   );
 }
