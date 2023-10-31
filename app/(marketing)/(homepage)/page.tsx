02dcd0400568879a8880e1f0979d62c67968ac31
gio <gio>
2023-10-31 18:28:09 +0800

    delete page that created conflict

--- app/(marketing)/(homepage)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Crypto Data Viz is still under construction :)</p>
-      <p>Please come back later!</p>
-      <p>this shouldn't exist in main</p>
-    </main>
-  );
-}
