268cd6fc1ce827228e083da354a467f2fc31c803
gio <gio>
2023-10-31 16:19:55 +0800

    new text added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Crypto Data Viz is still under construction :))
+      <p>Crypto Data Viz is still under construction :)</p>
+      <p>Please come back later!</p>
     </main>
   );
 }
