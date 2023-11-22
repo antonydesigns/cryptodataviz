715707669f926078a6d9eda6facf48df56e1ec35
gio <gio>
2023-11-22 18:03:54 +0800

    final

--- app/(pages)/framer-motion/page.tsx
+++ app/(pages)/framer-motion/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { MotionValue, motion, useSpring, useTransform } from "framer-motion";
+import {
+  MotionValue,
+  motion,
+  useSpring,
+  useTransform,
+  useMotionValue,
+} from "framer-motion";
 
 export default function FramerMotionPage() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   const array = [];
   for (let i = 0; i < 10; i++) {
-    array.push(i + 1);
+    array.push(i);
   }
 
   const animatedValue = useSpring(count);
@@ -27,13 +33,13 @@ export default function FramerMotionPage() {
             Change count: {count}
           </button>
           <button
-            onClick={() => setCount(1)}
+            onClick={() => setCount(0)}
             className="py-2 px-3 ring-1 ring-black rounded-md mr-3 bg-red-200"
           >
             Reset
           </button>
         </div>
-        <div className="numbers w-[40px] h-[40px] relative ring-1 ring-black justify-center flex items-center">
+        <div className="numbers w-[40px] h-[40px] relative ring-1 ring-black justify-center flex items-center overflow-hidden">
           {array &&
             array?.map((number, idx) => (
               <Number number={number} mv={animatedValue} key={idx} />
@@ -45,18 +51,26 @@ export default function FramerMotionPage() {
 }
 
 function Number({ number, mv }: { number: number; mv: MotionValue }) {
-  let yOffset = useTransform(mv, (latest) => {
+  let position = useTransform(mv, (latest) => {
     let height = 40; // height of each counter block
-    let offset = number - latest;
+    let placeValue = latest % 10;
 
-    return offset * height;
+    let offset = (10 + number - placeValue) % 10;
+
+    let position = offset * height;
+
+    if (offset > 5) {
+      position = position - 10 * height;
+    }
+
+    return position;
   });
   return (
     <>
       <motion.span
         className="absolute"
         style={{
-          y: yOffset,
+          y: position,
         }}
       >
         {number}
