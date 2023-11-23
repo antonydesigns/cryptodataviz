1a00392647fd558c5af030671787eafe01b2c025
gio <gio>
2023-11-23 15:52:57 +0800

    add logic to generate random names

--- app/(pages)/framer-motion/page.tsx
+++ app/(pages)/framer-motion/page.tsx
@@ -7,15 +7,52 @@ import {
   useSpring,
   useTransform,
   useMotionValue,
+  AnimatePresence,
 } from "framer-motion";
 
 export default function FramerMotionPage() {
+  const [nameList, setNameList] = useState<string[]>([]);
 
+  const firstNames = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "Daniel",
+    "Farah",
+    "Elanor",
+    "George",
+    "Hannah",
+  ];
 
-  return (
-    <>
-   
-    </>
-  );
+  const lastNames = [
+    "Atkinson",
+    "Boyle",
+    "Clarke",
+    "Dresner",
+    "Einstein",
+    "Faraday",
+    "Gryffindor",
+    "Hoskinson",
+  ];
+
+  function generateName() {
+    let index1 = Math.floor(Math.random() * firstNames.length);
+    let index2 = Math.floor(Math.random() * lastNames.length);
+    return `${firstNames[index1]} ${lastNames[index2]}`;
+  }
+
+  let list: string[] = [];
+  function generateNameList() {
+    for (let i = 0; i < 10; i++) {
+      nameList.push(generateName());
+    }
+    setNameList(list);
+  }
+
+  useEffect(() => {
+    generateNameList();
+    console.log(nameList);
+  }, []);
+
+  return <></>;
 }
-
