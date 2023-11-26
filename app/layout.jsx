829c34ad1f0a92175340ace2f40dc2c08c3ce219
gio <gio>
2023-11-26 17:27:36 +0800

    fixed error

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Link from "next/link";
-import Amplify from "aws-amplify";
-import awsConfig from "./aws-exports";
+import { Amplify } from "aws-amplify";
+import awsConfig from "@/src/aws-exports";
 
 Amplify.configure(awsConfig);
 
